d36e0b5d8df0ed74eb93331636a20a5152e78d13
Alexander Chneerov <achneerov@gmail.com>
2025-05-24 18:11:09 -0400

    s

--- src/app/component/body/body.component.ts
+++ src/app/component/body/body.component.ts
@@ -2,10 +2,11 @@ import { Component } from "@angular/core";
 import { ConsoleComponent } from "../console/console.component";
 import { DescriptionComponent } from "../description/description.component";
 import { IdeComponent } from "../ide/ide.component";
+import { AngularSplitModule } from 'angular-split';
 
 @Component({
   selector: "app-body",
-  imports: [ConsoleComponent, DescriptionComponent, IdeComponent],
+  imports: [ConsoleComponent, DescriptionComponent, IdeComponent, AngularSplitModule],
   templateUrl: "./body.component.html",
   styleUrl: "./body.component.scss",
 })
