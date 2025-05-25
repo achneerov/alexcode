074868c2b8a770d2faa6376912f1dc3004c2af8c
Alexander Chneerov <achneerov@gmail.com>
2025-05-24 20:33:25 -0400

    s

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatInputModule, FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  searchText: string = '';
 }
