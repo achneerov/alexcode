b5f3da09a56890caafe4fee5613d665f4c7e0ef9
Alexander Chneerov <achneerov@gmail.com>
2025-05-27 21:11:43 -0400

    s

--- src/app/component/ide/ide.component.ts
+++ src/app/component/ide/ide.component.ts
@@ -1,11 +1,29 @@
-import { Component } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { MatInputModule } from "@angular/material/input";
 import { MatFormFieldModule } from "@angular/material/form-field";
+import { Problem } from "../../interface/problem.interface";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "app-ide",
-  imports: [MatInputModule, MatFormFieldModule],
+  standalone: true,
+  imports: [MatInputModule, MatFormFieldModule, CommonModule],
   templateUrl: "./ide.component.html",
   styleUrl: "./ide.component.scss",
 })
-export class IdeComponent {}
+export class IdeComponent implements OnInit {
+  @Input() problemData: Problem | undefined;
+  functionTemplate: string = "";
+
+  ngOnInit(): void {
+    this.generateFunctionTemplate();
+  }
+
+  generateFunctionTemplate(): string {
+    if (this.problemData && this.problemData.function_params_names) {
+      const paramList = this.problemData.function_params_names.join(", ");
+      return `def function(${paramList}):`;
+    }
+    return "";
+  }
+}
