8cff8175838df55926a8df05c27643df71423587
Alexander Chneerov <achneerov@gmail.com>
2025-05-31 22:32:49 -0400

    s

--- src/app/component/description/description.component.ts
+++ src/app/component/description/description.component.ts
@@ -1,13 +1,27 @@
 import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { Problem, TestCase } from "../../interface/problem.interface";
+import { DomSanitizer, SafeHtml } from "@angular/platform-browser";
 
 @Component({
   selector: "app-description",
+  standalone: true,
   imports: [CommonModule],
   templateUrl: "./description.component.html",
   styleUrl: "./description.component.scss",
 })
-export class DescriptionComponent {
+export class DescriptionComponent implements OnChanges {
   @Input() problemData: Problem | undefined;
+  sanitizedDescription: SafeHtml = "";
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    // When problemData changes, sanitize the HTML description
+    if (this.problemData && this.problemData.description) {
+      this.sanitizedDescription = this.sanitizer.bypassSecurityTrustHtml(
+        this.problemData.description,
+      );
+    }
+  }
 }
