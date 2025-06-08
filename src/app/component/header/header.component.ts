9f8bb1740dd547e260f2f42ab77d6d8c9406717e
Alexander Chneerov <achneerov@gmail.com>
2025-06-08 16:19:48 -0400

    s

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -6,6 +6,8 @@ import { MatInputModule } from "@angular/material/input";
 import { FormsModule } from "@angular/forms";
 import { Router } from "@angular/router";
 import { CodeExecutionService } from "../../service/code-execution.service";
+import { CommonModule } from "@angular/common";
+import { ProblemDataService } from "../../service/problem-data.service";
 
 @Component({
   selector: "app-header",
@@ -16,16 +18,19 @@ import { CodeExecutionService } from "../../service/code-execution.service";
     MatIconModule,
     MatInputModule,
     FormsModule,
+    CommonModule,
   ],
   templateUrl: "./header.component.html",
   styleUrl: "./header.component.scss",
 })
 export class HeaderComponent {
   searchText: string = "";
+  showErrorMessage: boolean = false;
 
   constructor(
     private codeExecutionService: CodeExecutionService,
     private router: Router,
+    private problemDataService: ProblemDataService,
   ) {}
 
   runCode(): void {
@@ -35,7 +40,21 @@ export class HeaderComponent {
   goToProblem(): void {
     // Default to problem 0 if searchText is empty
     const problemId = this.searchText ? this.searchText : "0";
-    this.router.navigate(["/problem", problemId]);
+
+    // Check if problem exists
+    this.problemDataService.getProblemData(problemId).subscribe({
+      next: () => {
+        // Problem exists, navigate to it
+        this.router.navigate(["/problem", problemId]);
+      },
+      error: () => {
+        // Problem doesn't exist, show error message
+        this.showErrorMessage = true;
+        setTimeout(() => {
+          this.showErrorMessage = false;
+        }, 1000); // Hide after 1 second
+      },
+    });
   }
 
   // Handle Enter key in search input
